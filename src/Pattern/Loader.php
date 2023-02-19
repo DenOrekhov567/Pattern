ea622e649856887101923a93a6f9d4db39fdaf28
Денис Орехов <103766080+DenOrekhov567@users.noreply.github.com>
2023-02-19 13:12:42 +0300

    Add files via upload

---
+++ src/Pattern/Loader.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pattern;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\Server;
+
+class Loader extends PluginBase
+{
+
+	public function onEnable(): void {
+
+	}
+
+}
