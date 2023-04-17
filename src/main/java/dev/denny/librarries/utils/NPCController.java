5e5d0eae296c080a250d66562eabd829fcde7615
Денис Орехов <103766080+DenOrekhov567@users.noreply.github.com>
2023-04-17 19:45:57 +0300

    Delete src/main directory
    Refactoring code

--- src/main/java/dev/denny/librarries/utils/NPCController.java
+++
@@ -1,9 +0,0 @@
-package dev.denny.librarries.utils;
-
-import cn.nukkit.Player;
-import dev.denny.librarries.entity.NPC;
-
-public interface NPCController {
-
-    void handle(NPC clickedNPC, Player player);
-}
